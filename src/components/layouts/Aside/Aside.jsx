17a0d07eaa83ac399fbd909f381113e77c398718
루밀LuMir <rpfos@naver.com>
2024-11-19 21:59:31 +0900

    feat: sidebar toggle button in mobile environment (#161)
    * wip
    
    * wip

--- src/components/layouts/Aside/Aside.jsx
+++ src/components/layouts/Aside/Aside.jsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+import { HiOutlineMenuAlt2 } from 'react-icons/hi';
+
 import styles from './Aside.module.scss';
 
 export default function Aside({ children }) {
-  return <aside className={styles.aside}>{children}</aside>;
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <aside className={`${styles.aside} ${visible ? styles.visible : ''}`}>
+        {children}
+      </aside>
+      <div
+        className={`${styles.div} ${visible ? styles.visible : ''}`}
+        onClick={() => {
+          setVisible(prevState => !prevState);
+        }}
+      >
+        <HiOutlineMenuAlt2 />
+      </div>
+    </>
+  );
 }
+
+// TODO: Add `useMemo` Hook to `${visible ? styles.visible : ''}`.
