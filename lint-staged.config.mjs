b7992206545d55e7b165b96592a588879de2ed50
루밀LuMir <rpfos@naver.com>
2025-04-17 21:59:40 +0900

    chore(*): update `lint-staged` and scripts

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,11 @@
 export default {
   '*': [
-    'npx prettier --check --ignore-unknown',
+    'npx prettier --write --ignore-unknown',
     'npx editorconfig-checker -config .editorconfig-checker.json',
   ],
-  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md}': 'npx eslint',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md}': 'npx eslint --fix',
   '*.{css,scss}': 'npx stylelint',
-  '*.md': 'npx markdownlint',
+  '*.md': 'npx markdownlint --fix',
   'src/docs/**/*.md': 'npx textlint -f pretty-error',
   '*.{h,c,cpp}': 'npx clang-format -n -Werror',
 };
