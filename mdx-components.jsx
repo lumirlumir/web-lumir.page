3913dd707b513bf54784564f0b3d877faebc21d4
루밀LuMir <rpfos@naver.com>
2025-07-01 14:49:57 +0900

    feat(*): integrate MDX (#405)
    This pull request introduces MDX support in the Next.js application,
    enabling the use of MDX files for enhanced content rendering. Key
    changes include the addition of MDX-related dependencies, configuration
    updates to integrate MDX, and the creation of a sample MDX page.
    
    ### MDX Integration:
    
    *
    [`mdx-components.jsx`](diffhunk://#diff-4c9a06b00260df2ee2d97ed551cb1651f88be7e6b73eace119ffa2204fc6c8b8R1-R12):
    Added a `useMDXComponents` function to allow customization of built-in
    MDX components. This file is required for MDX usage in the `app`
    directory.
    *
    [`next.config.mjs`](diffhunk://#diff-18c049b08c4a0f5ab451c598aeb2c4848bb9d7877b51ca3e5effb94a225814d2R10-R20):
    Integrated MDX support by adding the `@next/mdx` package, defining MDX
    file extensions, and exporting the configuration using the `withMDX`
    wrapper. Included references to relevant documentation for MDX setup.
    [[1]](diffhunk://#diff-18c049b08c4a0f5ab451c598aeb2c4848bb9d7877b51ca3e5effb94a225814d2R10-R20)
    [[2]](diffhunk://#diff-18c049b08c4a0f5ab451c598aeb2c4848bb9d7877b51ca3e5effb94a225814d2R44-R67)
    
    ### Dependency Updates:
    
    *
    [`package.json`](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519R33-R35):
    Added MDX-related dependencies (`@mdx-js/loader`, `@mdx-js/react`,
    `@next/mdx`) and type definitions (`@types/mdx`, `@types/react`,
    `@types/react-dom`). Updated the `typescript` version to `^5.8.3`.
    [[1]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519R33-R35)
    [[2]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519R50-R53)
    [[3]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L64-R71)
    
    ### Example MDX Page:
    
    *
    [`src/app/posts/temp/page.md`](diffhunk://#diff-203af7dffc50728162d4460543a5058eeb594a1c2cd750f314de4d8d9002d95cR1-R3):
    Created a simple "Hello World" MDX page to demonstrate the
    functionality.

---
+++ mdx-components.jsx
@@ -0,0 +1,12 @@
+// import type { MDXComponents } from 'mdx/types';
+
+/* eslint-disable import/prefer-default-export, react/jsx-filename-extension */
+
+// This file is required to use MDX in `app` directory.
+export function useMDXComponents(components) {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    ...components,
+  };
+}
