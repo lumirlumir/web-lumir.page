9ee3fa6deccdd8993e94e0c2658e5970e2b78cad
루밀LuMir <rpfos@naver.com>
2026-04-02 23:51:49 +0900

    wip

--- apps/front-blog/vitest.config.js
+++ apps/front-blog/vitest.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    alias: {
-      '@': './src',
-    },
     include: ['src/**/*.test.{js,mjs,cjs,jsx,ts,mts,cts,tsx}'],
   },
 });
