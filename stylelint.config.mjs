68674cdd343768346fd4c5214e521c4dffe55d60
루밀LuMir <rpfos@naver.com>
2026-04-05 21:30:18 +0900

    wip

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
-  ignoreFiles: ['archives/**'],
+  ignoreFiles: ['archives/**', '**/coverage/**'],
   rules: {
     'scss/no-global-function-names': null,
+
+    // Enforce specific media feature breakpoints for consistency
+    'media-feature-range-notation': 'context',
+    'media-feature-name-allowed-list': ['width'],
+    'media-feature-name-unit-allowed-list': {
+      width: ['px'],
+    },
+    'media-feature-name-value-allowed-list': {
+      width: ['640px', '768px', '1024px', '1280px', '1536px'],
+    },
   },
 };
